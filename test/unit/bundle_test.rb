27f7eb9e4867bd85cc6881a3163b35f9767db56f
Janoo Fernandes <smoothclip@gmail.com>
2015-01-06 11:10:46 -0500

    Add static Bundle loading test

---
+++ test/unit/bundle_test.rb
@@ -0,0 +1,18 @@
+require_relative '../test_helper'
+
+class BundleTest < Test::Unit::TestCase
+
+  def test_example_bundle
+    client = FHIR::Client.new("feed-test")
+    root = File.expand_path '..', File.dirname(File.absolute_path(__FILE__))
+    bundle_xml = File.read(File.join(root, 'fixtures', 'bundle-example.xml'))
+    response = RestClient::Response.create(bundle_xml, {}, {})
+    def response.code; 200; end
+    clientReply = FHIR::ClientReply.new('feed-test', response)
+
+    bundle = client.parse_reply(FHIR::Bundle, FHIR::Formats::FeedFormat::FEED_XML, clientReply.body)
+
+    assert !bundle.blank?, "Failed to parse example Bundle."
+  end
+
+end
