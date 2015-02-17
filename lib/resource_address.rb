3d2edec82803bd47afceaee0e0717eb769d387c7
Janoo Fernandes <smoothclip@gmail.com>
2015-02-17 11:46:33 -0500

    Allow resource specifications to use Strings in addition to Classes

--- lib/resource_address.rb
+++ lib/resource_address.rb
@@ -67,7 +67,8 @@ module FHIR
 
       params = {}
       url = ""
-      url += "/#{options[:resource].name.demodulize}" if options[:resource]
+      # handle requests for resources by class or string; useful for testing nonexistent resource types
+      url += "/#{ options[:resource].try(:name).try(:demodulize) || options[:resource].split("::").last }" if options[:resource]
       url += "/_validate" if options[:validate]
       url += "/#{options[:id]}" if options[:id]
 
