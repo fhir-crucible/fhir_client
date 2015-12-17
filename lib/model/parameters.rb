29678ac99f3dd634094de875b270d76f6851092a
Jason Walonoski <jwalonoski@mitre.org>
2015-12-17 11:14:04 -0500

    Add deserialization to Parameters.

--- lib/model/parameters.rb
+++ lib/model/parameters.rb
@@ -66,5 +66,72 @@ module FHIR
       JSON.pretty_unparse(hash)
     end
 
+    def self.from_fhir_json(json)
+       # if the input is a String, convert it into a Hash
+      if json.is_a? String
+        begin
+          if json.encoding.names.include? 'UTF-8'
+            json.gsub!("\xEF\xBB\xBF".force_encoding('UTF-8'), '') # remove UTF-8 BOM
+          end
+          hash = JSON.parse(json)
+        rescue Exception => e
+          $LOG.error "Failed to parse JSON hash as resource: #{e.message} %n #{json} %n #{e.backtrace.join("\n")}"
+          return nil
+        end
+      end
+      
+      resourceType = hash['resourceType']
+      return FHIR::Resource.from_contents(json) if !resourceType.nil? && resourceType!='Parameters'
+
+      obj = FHIR::Parameters.new
+      hash['parameter'].each do |phash|
+        pname = phash['name']
+        presource = phash['resource']
+        if !presource.nil?
+          presource = FHIR::Resource.from_contents(JSON.pretty_unparse(presource))
+          obj.add_resource_parameter(pname,presource)
+        else
+          key = phash.keys.select{|k|k.start_with?'value'}.first
+          pvalue = phash[key]
+          pvalueType = key[5..-1]
+          obj.add_parameter(pname,pvalueType,pvalue)
+        end
+      end
+      obj
+    end
+
+    def self.from_xml(xml)
+      doc = Nokogiri::XML(xml)
+      doc.root.add_namespace_definition('fhir', 'http://hl7.org/fhir')
+      doc.root.add_namespace_definition('xhtml', 'http://www.w3.org/1999/xhtml')
+      entry = doc.at_xpath("./fhir:#{self.name.demodulize}")
+      
+      obj = FHIR::Parameters.new
+      entry.xpath('./fhir:parameter').each do |p|
+        pname = p.at_xpath('./fhir:name/@value').try(:value)
+        presource = p.at_xpath('./fhir:resource/*').try(:to_xml)
+        if !presource.nil?
+          rdoc = Nokogiri::XML(presource)
+          add_namespace_definition(rdoc.root)
+          presource = FHIR::Resource.from_contents(rdoc.to_xml)
+          obj.add_resource_parameter(pname,presource)
+        else
+          key = p.xpath('./*').select{|x|x.name.start_with?'value'}.try(:first).try(:name)
+          pvalue = p.at_xpath("./fhir:#{key}/@value").try(:value)
+          pvalueType = key[5..-1]
+          obj.add_parameter(pname,pvalueType,pvalue)
+        end
+      end
+      obj
+    end
+
+    def self.add_namespace_definition(element)
+      element.namespace=element.add_namespace_definition('fhir', 'http://hl7.org/fhir')
+      element.add_namespace_definition('xhtml', 'http://www.w3.org/1999/xhtml')
+      element.children.select{|e|e.class==Nokogiri::XML::Element}.each do |e|
+        add_namespace_definition(e)
+      end
+    end
+
   end
 end
---
+++ test/unit/parameters_test.rb
@@ -0,0 +1,45 @@
+require_relative '../test_helper'
+
+class ParametersTest < Test::Unit::TestCase
+# <?xml version="1.0" encoding="UTF-8"?>
+# <Parameters xmlns="http://hl7.org/fhir">
+#   <id value="example"/>
+#   <parameter>
+#     <name value="start"/>
+#     <valueDate value="2010-01-01"/>
+#   </parameter> 
+#   <parameter>
+#     <name value="end"/>
+#     <resource>
+#       <Binary>
+#         <contentType value="text/plain"/>
+#         <content value="VGhpcyBpcyBhIHRlc3QgZXhhbXBsZQ=="/>
+#       </Binary>
+#     </resource>
+#   </parameter> 
+# </Parameters>
+  def test_example_parameters_xml
+    root = File.expand_path '..', File.dirname(File.absolute_path(__FILE__))
+    xml = File.read(File.join(root, 'fixtures', 'parameters-example.xml'))
+    parameters = FHIR::Parameters.from_xml(xml)
+    check_params(parameters)
+   end
+
+  def test_example_parameters_json
+    root = File.expand_path '..', File.dirname(File.absolute_path(__FILE__))
+    json = File.read(File.join(root, 'fixtures', 'parameters-example.json'))
+    parameters = FHIR::Parameters.from_fhir_json(json)
+    check_params(parameters)
+  end
+
+  def check_params(parameters)
+    message = "Failed to parse example Parameters."
+    assert parameters.parameter.length==2, message
+    assert parameters.parameter[0].name=='start', message
+    assert parameters.parameter[0].valueType=='Date', message
+    assert parameters.parameter[0].value=='2010-01-01', message
+    assert parameters.parameter[1].name=='end', message
+    assert !parameters.parameter[1].resource.nil? && parameters.parameter[1].resource.class==FHIR::Binary, message
+  end
+
+end
