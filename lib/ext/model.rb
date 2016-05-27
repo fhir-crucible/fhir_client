6d85cd8c92432e3eacedecb7c3b37a526726b9fc
Rob Scanlon <robscanlon@gmail.com>
2016-05-27 13:53:37 -0400

    Allow client to be passed to api explicitly

--- lib/ext/model.rb
+++ lib/ext/model.rb
@@ -1,9 +1,10 @@
 module FHIR
   class Model
 
+    attr_accessor :client
+
     class << self
       cattr_accessor :configuration
-      cattr_accessor :last_response
     end
 
     def self.configure
@@ -13,55 +14,68 @@ module FHIR
 
     # class methods 
 
-    def self.find(id)
-      self.last_response = self.configuration.client.read(self, id)
-      self.last_response.resource
+    def self.find(id, options = {})
+      client = self.pull_client_from options
+      response = client.read(self, id)
+      response.resource.client = client
+      response.resource
     end
 
-    def self.all()
-      self.last_response = self.configuration.client.read_feed(self)
-      self.last_response.resource
+    def self.all(options = {})
+      client = pull_client_from options
+      response = client.read_feed(self)
+      response.resource.client = client
+      response.resource
     end
 
-    def self.create(resource_hash)
-      resource = self.new.from_hash(resource_hash)
-      self.last_response = self.configuration.client.create(resource)
-      self.last_response.resource
+    def self.create(options)
+      client = pull_client_from options
+      resource = self.new.from_hash(options)
+      response = client.create(resource)
+      response.resource.client = client
+      response.resource
     end
 
-    def self.destroy(id)
-      self.last_response = self.configuration.client.destroy(self, id)
+    def self.destroy(id, options = {})
+      client = pull_client_from options
+      response = client.destroy(self, id)
       nil
     end
 
-    def self.where(parameters)
-      options = { search: { parameters: parameters }}
-      self.last_response = self.configuration.client.search(self, options)
-      self.last_response.resource
+    def self.where(options)
+      client = pull_client_from options
+
+      options = { search: { parameters: options }}
+      response = client.search(self, options)
+      response.resource.client = client
+      response.resource
     end
 
     # instance methods
 
-    def save()
+    def save(options = {})
+      client = self.class.pull_client_from options, @client
       if self.id.nil?
-        self.class.last_response = self.class.configuration.client.create(self)
+        last_response = client.create(self)
       else
-        self.class.last_response = self.class.configuration.client.update(self, self.id)
+        last_response = client.update(self, self.id)
       end
-      self.class.last_response.resource
+      last_response.resource
     end
 
-    def destroy()
-      self.class.destroy(self.id) unless self.id.nil?
+    def destroy(options = {})
+      client = self.class.pull_client_from options, @client
+      self.class.destroy(self.id, client: client) unless self.id.nil?
+    end
+
+    private
+
+    def self.pull_client_from(options, instance_client = nil)
+      options.delete(:client) || instance_client || self.configuration.client
     end
 
     class Configuration
       attr_accessor :client
-      attr_accessor :format
-
-      def initialize
-        @format = FHIR::Formats::ResourceFormat::RESOURCE_XML
-      end
     end
 
   end
--- lib/tasks/tasks.rake
+++ lib/tasks/tasks.rake
@@ -9,28 +9,46 @@ namespace :fhir do
   task :api_testing, [] do |t, args|
 
     client = FHIR::Client.new 'http://fhirtest.uhn.ca/baseDstu3'
-    # client = FHIR::Client.new 'http://sqlonfhir-may.azurewebsites.net/fhir'
+    client2 = FHIR::Client.new 'http://sqlonfhir-may.azurewebsites.net/fhir'
 
     FHIR::Model.configure { |c| c.client = client }
 
-    patient = FHIR::Patient.create(name: {given: 'Joe', family: 'Smith'})
+    patient1 = FHIR::Patient.create(name: {given: 'Joe', family: 'Smith'})
+    patient2 = FHIR::Patient.create(name: {given: 'Joe', family: 'Smith'}, client: client2)
 
     results = FHIR::Patient.where(given: 'Joe', family: 'Smith')
+    results = FHIR::Patient.where(given: 'Joe', family: 'Smith', client: client2)
 
-    patient.name[0].family = 'Smithfield'
-    patient.save
-
-    patient_check = FHIR::Patient.find(patient.id)
-
-    patient2 = FHIR::Patient.new(name: {given: 'Sam', family: 'Jones'})
+    patient1.save
+    patient2.name[0].family = 'Smithfield'
     patient2.save
 
-    all_patients = FHIR::Patient.all
-    # all_patients.count() # enumerable
-    binding.pry
+    patient_check = FHIR::Patient.find(patient1.id)
+    patient_check = FHIR::Patient.find(patient1.id, client: client)
 
-    patient.destroy
+    patient3 = FHIR::Patient.new(name: {given: 'Sam', family: 'Jones'})
+    patient3.save(client:client)
+    patient4 = FHIR::Patient.new(name: {given: 'Sam', family: 'Jones'})
+    patient4.save(client: client2)
+    patient_check = FHIR::Patient.find(patient1.id)
+
+    all_patients = FHIR::Patient.all()
+    all_patients = FHIR::Patient.all(client: client2)
+
+    # all_patients.count() # enumerable
+
+    patient1.destroy
     patient2.destroy
+    patient3.destroy
+
+  end
+
+  desc 'WIP to test the new API; delete when done'
+  task :api_testing_noconfig, [] do |t, args|
+
+    client = FHIR::Client.new 'http://fhirtest.uhn.ca/baseDstu3'
+    patient = FHIR::Patient.create(name: {given: 'Joe', family: 'Smith'}, client: client)
+    patient.destroy
 
   end
 
