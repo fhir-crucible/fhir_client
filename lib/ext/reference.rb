c6a32f8fd473c31d7af9f0074231be3a55607d37
Andrew Ross <andrew@vermonster.com>
2016-06-30 12:02:43 -0400

    add helper to resolve contained references (and fix bug)

--- lib/ext/reference.rb
+++ lib/ext/reference.rb
@@ -1,11 +1,28 @@
 module FHIR
   class Reference
+    def contained?
+      reference.to_s.start_with?('#')
+    end
+
+    def id
+      if contained?
+        reference.to_s[1..-1]
+      else
+        reference.to_s.split('/').last
+      end
+    end
+
+    def type
+      reference.to_s.split('/').first unless contained?
+    end
+
+    def resource_class
+      "FHIR::#{type}".constantize unless contained?
+    end
+
     def read
-      # TODO: how to follow contained references?
-      type, id = reference.to_s.split("/")
-      return unless [type, id].all?(&:present?)
-      klass = "FHIR::#{type}".constantize
-      klass.read(id, client)
+      return if contained? || type.blank? || id.blank?
+      resource_class.read(id, client)
     end
   end
 end
