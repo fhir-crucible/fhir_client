832989da4abed90f7812075894533ab4e770dfcb
Jason Walonoski <jwalonoski@mitre.org>
2016-10-25 11:10:04 -0400

    Remove dependency on external server for unit tests.

--- test/unit/basic_test.rb
+++ test/unit/basic_test.rb
@@ -1,16 +1,10 @@
 require_relative '../test_helper'
-WebMock.allow_net_connect!
 
 class BasicTest < Test::Unit::TestCase
-  TESTING_ENDPOINT = 'http://fhirtest.uhn.ca/baseDstu3'.freeze
 
   def test_client_initialization
-    client = FHIR::Client.new(TESTING_ENDPOINT)
+    client = FHIR::Client.new('TESTING_ENDPOINT')
     assert !client.use_format_param, 'Using _format instead of [Accept] headers.'
   end
 
-  def test_conformance
-    client = FHIR::Client.new(TESTING_ENDPOINT)
-    assert !client.conformance_statement.nil?, 'Unable to retrieve conformance statement.'
-  end
 end
