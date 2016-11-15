17bcaf77a978eb807d133cad4a5338ae2faad201
Wes Rich <wes@wesrich.com>
2016-11-15 17:58:04 -0500

    CodeClimate Test Reporter Deprecation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 require 'simplecov'
+SimpleCov.start
+
 require 'pry'
 require 'test/unit'
 require 'webmock/test_unit'
