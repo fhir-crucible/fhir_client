d8763ce021f7f6dd965c050ad6665a1194d81d84
Rob Dingwell <rob@robdingwell.org>
2018-06-01 13:02:05 -0400

    adding some comments

--- test/unit/client_acess_token_test.rb
+++ test/unit/client_acess_token_test.rb
@@ -7,7 +7,9 @@ class ClientAccessTokenTest < Test::Unit::TestCase
   end
 
   def test_can_configure_client_with_access_token_authentication
-
+    # stub out a request for a resource, once configured with the access token
+    # the auth methed would be a bearer token with the provided access token
+    # the stub only works on calls that have the correct authentication header
     stub_request(:get, /Patient\/example/).
                         with(headers:{"Authorization"=>"Bearer some token"}).
                         to_return(body: "{}", headers: {"Content-Type" => "application/json"} )
@@ -21,7 +23,15 @@ class ClientAccessTokenTest < Test::Unit::TestCase
 
   def test_can_configure_client_with_refresh_token_for_authentication
     new_token = "{\"access_token\": \"New access Token\"}"
+    # stub request for requesting a new access token from an auth server token endpoint
+    #   this will return the expected new access token
     stub_request(:post, /auth\/token/).to_return(body: new_token, headers: {"Content-Type" => "application/json"} )
+
+    # stub out a request for a resource, once configured with the access token
+    # the auth methed would be a bearer token with the access token retrieved
+    # from the previous call to get a new access token
+    # the stub only works on calls that have the correct authentication header
+
     stub_request(:get, /Patient\/example/).
                         with(headers:{"Authorization"=>"Bearer New access Token"}).
                         to_return(body: "{}", headers: {"Content-Type" => "application/json"} )
@@ -30,10 +40,15 @@ class ClientAccessTokenTest < Test::Unit::TestCase
                                                        expires_in: -1,
                                                        refresh_token: "My Refresh Token",
                                                        token_path: "/auth/token"});
+
     assert_equal "New access Token", token.token
+    # make a call to test the stubbed out method with auth type
     assert_equal "{}", client.read(FHIR::Patient, "example").response[:body]
   end
 
+  # Need to provider at a minimum an access_token or a refresh_token, it can be
+  # both but need atleast one of them, otherwise there is nothing to configure
+  # against
   def test_must_supply_either_an_access_token_or_a_refresh_token
     begin
       client.set_auth_from_token("id","secret", {})
@@ -43,6 +58,8 @@ class ClientAccessTokenTest < Test::Unit::TestCase
     end
   end
 
+  # Make sure that we can auto configure the auth and token endpoints from a
+  # capability statement.  
   def test_can_configure_access_token_with_auto_configure
     root = File.expand_path '..', File.dirname(File.absolute_path(__FILE__))
     capabilitystatement = File.read(File.join(root, 'fixtures', 'oauth_capability_statement.json'))
