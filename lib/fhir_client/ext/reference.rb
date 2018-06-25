ec67b3487ab5903187a8ea9c39a8cdddce63a472
Etienne Depaulis <etienne.depaulis@gmail.com>
2018-06-25 09:34:58 +0200

    Added Reference.vread logic

--- lib/fhir_client/ext/reference.rb
+++ lib/fhir_client/ext/reference.rb
@@ -4,9 +4,16 @@ module FHIR
       reference.to_s.start_with?('#')
     end
 
+    def has_version?
+      /history/.match reference.to_s
+    end
+
+
     def reference_id
       if contained?
         reference.to_s[1..-1]
+      elsif has_version?
+        reference.to_s.split('/').second
       else
         reference.to_s.split('/').last
       end
@@ -16,11 +23,25 @@ module FHIR
       reference.to_s.split('/').first unless contained?
     end
 
+    def version_id
+      if has_version?
+         reference.to_s.split('/').last
+      end
+    end
+
+
     def read
       return if contained? || type.blank? || (id.blank? && reference.blank?)
       rid = id || reference_id
       resource_class.read(rid, client)
     end
+
+    def vread
+      return if contained? || type.blank? || (id.blank? && reference.blank?) || version_id.blank?
+      rid = id || reference_id
+      resource_class.vread(rid, version_id, client)
+    end
+
   end
 end
 
@@ -38,7 +59,7 @@ module FHIR
   module DSTU2
     class Reference
       include FHIR::ReferenceExtras
-      
+
       def resource_class
         "FHIR::DSTU2::#{type}".constantize unless contained?
       end
